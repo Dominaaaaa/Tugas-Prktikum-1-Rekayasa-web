2e57d2856945419ec68ab06185dacda8ee92c274
Dominaaaaa <kfirman253@gmail.com>
2024-10-05 22:28:33 +0700

    Create json to array.php
    Nama: Firman Kurniawan
    Nim: G.231.22.0019

---
+++ json to array.php
@@ -0,0 +1,16 @@
+<?php
+
+// Membuat array variabel dari JSON
+$jsonData = '{"nama":"Firman","usia":20,"pekerjaan":"Mahasiswa","hobi":["Membaca","Bersepeda","Coding"]}';
+
+$arraydata = json_decode($jsonData, true);
+
+// Menampilkan data dengan label
+echo "Nama: " . $arraydata["nama"] . "<br>";
+echo "Usia: " . $arraydata["usia"] . "<br>";
+echo "Pekerjaan: " . $arraydata["pekerjaan"] . "<br>";
+
+// Menggabungkan elemen array 'hobi' menjadi string dan menampilkannya
+echo "Hobi: " . implode(", ", $arraydata["hobi"]) . "<br>";
+
+?>
